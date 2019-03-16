6c0b5b39d7237aa7ecc2d0e0c1e8e38ca2d458c8
Andrey Novikov <andrey.novikov.dev@gmail.com>
2019-03-16 21:33:46 +0300

    Add auth manager

---
+++ src/managers/authManager.js
@@ -0,0 +1,48 @@
+import { ENDPOINTS } from '../api'
+
+const getTokens = (url, username, password) =>
+  fetch(url, {
+    method: 'POST',
+    body: JSON.stringify({ username, password }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then(res => {
+      if (res.status === 200 || res.status === 400) {
+        return Promise.all([res.status, res.json()])
+      }
+
+      return Promise.all([
+        res.status,
+        { errors: { nonField: ['Unknown error'] } },
+      ])
+    })
+    .then(([status, json]) => {
+      if (status === 200) {
+        return {
+          ok: true,
+          payload: {
+            accessToken: json.access,
+            refreshToken: json.refresh,
+          },
+        }
+      }
+
+      const { non_field_errors: nonField, ...fieldErrors } = json
+
+      return {
+        ok: false,
+        errors: {
+          nonField,
+          ...fieldErrors,
+        },
+      }
+    })
+    .catch(() => ({ ok: false, errors: { nonField: ['Unknown error'] } }))
+
+export const signUp = (username, password) =>
+  getTokens(ENDPOINTS.signUp, username, password)
+
+export const signIn = (username, password) =>
+  getTokens(ENDPOINTS.getToken, username, password)
