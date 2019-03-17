55cc1a4b3f108fbfc8d310a5d1a2fae8abbd6f9b
Andrey Novikov <andrey.novikov.dev@gmail.com>
2019-03-17 16:56:58 +0300

    Fix refresh token endpoint

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 export const ENDPOINTS = {
   signUp: '/auth/sign-up/',
   getToken: '/auth/token/',
-  refreshToken: '/auth/token/refresh/',
+  refreshToken: '/auth/token/refresh',
   accounts: '/accounts/',
   transactions: '/accounts/transactions/',
 }
